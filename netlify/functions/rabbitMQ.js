b40b9784cef5ffaefad19ae1b1c73f86438b7d08
Luis-12 <55066676+Luis-12@users.noreply.github.com>
2021-11-26 14:39:56 -0600

    fs

--- netlify/functions/rabbitMQ.js
+++ netlify/functions/rabbitMQ.js
@@ -2,7 +2,7 @@
 
 const amqp = require('amqplib');
 
-const CLOUDAMQP_URL = '***'
+const CLOUDAMQP_URL = 'amqps://jbspisew:codAlPoy4A1swX0ORL60nB1tpGcdAykj@fish.rmq.cloudamqp.com/jbspisew'
 
 module.exports = async() => { 
   const conn = await amqp.connect(CLOUDAMQP_URL);
