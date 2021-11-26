6340d39a96675d70d369ef34548dc496adf2c9cc
Luis-12 <55066676+Luis-12@users.noreply.github.com>
2021-11-26 14:25:21 -0600

    ew

--- netlify/functions/rabbitMQ.js
+++ netlify/functions/rabbitMQ.js
@@ -2,8 +2,10 @@
 
 const amqp = require('amqplib');
 
+const CLOUDAMQP_URL = '***'
+
 module.exports = async() => { 
-  const conn = await amqp.connect(process.env.CLOUDAMQP_URL);
+  const conn = await amqp.connect(CLOUDAMQP_URL);
   const channel = await conn.createChannel();
   return channel;
 }
