033748694d214a37a062ac27f7ff30f0431d36e6
Luis-12 <55066676+Luis-12@users.noreply.github.com>
2021-11-25 20:53:05 -0600

    s

--- netlify/functions/trackTasks.js
+++ netlify/functions/trackTasks.js
@@ -4,7 +4,7 @@ const rabbitPromise = require('./rabbitMQ');
 
 const headers = require('./headersCORS');
 
-const url = 'https://bookstore-rabbitmq.netlify.app/.netlify/functions/'
+const url = 'https://nervous-meitner-85e6fa.netlify.app/.netlify/functions/'
 
 exports.handler = async (event, context) => {
 
